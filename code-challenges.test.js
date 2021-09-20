7718f9a151eb1b81005a2a3c14913172df9cf8ae
LEARN Academy <learnacademy@LEARNs-Air.lan>
2021-09-20 07:02:23 -0700

    completed assesment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,16 +12,34 @@
 
 
 // --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
+funtion DivideBy = {(number) % 3===0)
+}
+// using funtion DivideBy to equal the number in array divided by three
+// return the number adding $ to add additional statement
 
+  return(`${number} is divisible by three`)
+}
 // a) Create a test with expect statements for each of the variables provided.
 
 var num1 = 15
+funtion DivideBy = {(num1) % 3===0)
+}
+  return(`${num1} is divisible by three`)
+}
 // Expected output: "15 is divisible by three"
 var num2 = 0
+
+funtion DivideBy = {(num2) % 3===0)
+}
+  return(`${num2} is divisible by three`)
+}
 // Expected output: "0 is divisible by three"
 var num3 = -7
+funtion DivideBy = {(num3) % 3===0)}
+{
+  return(`${num3} is not divisibleb by three`)
+}
 // Expected output: "-7 is not divisible by three"
-
 
 
 // b) Create the function that makes the test pass.
@@ -29,31 +47,65 @@ var num3 = -7
 
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
+//
 
+function capitalizedFirst = (randomNouns1) => {
+  return.charAt(0).toUpperCase()
+}
+//use funtion capitalized and name it capitalizedFirst to equal array randomNouns1
+//to equal return .charAt 0 representing first varible in string and add to toUpperCase the first
+//letter of each string
 // a) Create a test with expect statements for each of the variables provided.
 
 var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+let randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+let result = ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"] => {
+  return(capitalizedFirst(randomNouns1)).toUpperCase()
+}
+
 var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
-
-
+let randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
+let result = ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"] => {
+  return(capitalizedFirst(randomNouns2)).toUpperCase()
+}
 
 // b) Create the function that makes the test pass.
 
 
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
+let countVowels(string) {
+let count = 0
+  for (index i = 0; i string.length(); i++)
+  return count;
+}
+// use let to use function count a varible and renaming it countVowels to count string
+// make count variable 0 ,  index of length of string in array , make index at + 2
+// to return count
 
 // a) Create a test with expect statements for each of the variables provided.
 
 var vowelTester1 = "learn"
 // Expected output: 1
+let count = 0
+  for (index i = 0; i string.length("learn"); i++)
+  return count;
+}
+
 var vowelTester2 = "academy"
 // Expected output: 0
+let count = 0
+  for (index i = 0; i string.length("academy"); i++)
+  return count;
+}
 var vowelTester3 = "challenges"
 // Expected output: 2
-
+let count = 0
+  for (index i = 0; i string.length("challenges"); i++)
+  return count;
+}
 
 
 // b) Create the function that makes the test pass.
--- coding-concepts.js
+++ coding-concepts.js
@@ -7,53 +7,65 @@
 
 // --------------------1) What will this log?
 
-var cohort = "Delta 2021"
-// console.log(cohort.split(""))
+//var cohort = "Delta 2021"
+ //console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
-
+// a) Your answer: " D E L T A   2 0 2 1 "
+// b) Verify and explain:make cohort the varible to be equal to the string of Delta 2021 by putting Delta 2021 is "quotes".
+//by spliting cohort it will return the variables in the strings  "Delta 2021" spilt or spaced.
 
 // --------------------2) What will this log?
 
-const greeter = (name) => {
-  `Hello, ${name}!`
-}
-// console.log(greeter("LEARN Student"))
+ //const greeter = (name) => {
+  // `Hello, ${name}!`
+// }
+//console.log(greeter("LEARN Student"))
+//
+// // a) Your answer:"Hello, LEARN Student"
+// // b) Verify and explain:make greeter a varible by using constructor and equal by adding
+// = the same (value of array or "name")
+//to return that (value of array "name") as hello,value of array or "name" !
 
-// a) Your answer:
-// b) Verify and explain:
+//
+//
+// // --------------------3) What will this log?
+//
+// var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
+// // console.log(multBy2)
+//
+// // a) Your answer:[8, 10, 12, 14, 16]
+// // b) Verify and explain: make multBy2 equal the same as the value of variables inside of array by
+//maping the strings and multiping those values by 2
 
+//
+//
+// // --------------------4) What will this log?
+//
+// var oddsOnly = [11, 12, 13, 14, 15].filter(value => value % 2 === 0)
+// // console.log(oddsOnly)
+//
+// // a) Your answer:11, 13, 14, 15
+// // b) Verify and explain: make oddsOnly equal the same as the value of variables inside of array by
+// filtering the value of the strings located at location % by 2 to return only odd numbers
+//
+//
+// // --------------------5) What will this log?
+//
+// class Learn {
+//   constructor(name){
+//     this.student = name,
+//     this.cohort = "Delta",
+//     this.year = 2021
+//   }
+// }
+// var learnStudent = new Learn("George")
+// // console.log(learnStudent)
 
-// --------------------3) What will this log?
+// a) Your answer:"George, Delta 2021"
+// b) Verify and explain:make name a varible in array inside class learn.
+ By using constructor add to "name" array "Delta", and 2021 by
+ giving value with this. to strings student, cohort and year.
+ make a new varible learnStudent and have it equal new learn equal "george" for variable (name)
+// = the same (value of array or "name")
 
-var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
-// console.log(multBy2)
-
-// a) Your answer:
-// b) Verify and explain:
-
-
-// --------------------4) What will this log?
-
-var oddsOnly = [11, 12, 13, 14, 15].filter(value => value % 2 === 0)
-// console.log(oddsOnly)
-
-// a) Your answer:
-// b) Verify and explain:
-
-
-// --------------------5) What will this log?
-
-class Learn {
-  constructor(name){
-    this.student = name,
-    this.cohort = "Delta",
-    this.year = 2021
-  }
-}
-var learnStudent = new Learn("George")
-// console.log(learnStudent)
-
-// a) Your answer:
-// b) Verify and explain:
+//to return that (value of array "name") as hello,value of array or "name" !
